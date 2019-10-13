8f92c2eeb7329e881f2f5cbf13fc16db5a34131a
Michal Soucek <inasuk@gmail.com>
2019-10-14 00:01:40 +0200

    PhotoList now using BaseComponent too

--- frontend/app/components/PhotoTagger/PhotoList.tsx
+++ frontend/app/components/PhotoTagger/PhotoList.tsx
@@ -1,71 +1,58 @@
-import { h, render, Component } from "preact";
-import { route } from "preact-router";
-import { LeafletMouseEvent } from "leaflet";
+import { h, Component } from "preact";
 
-// components
-import { IDefProps } from "../../iface";
+import { IDefProps, IDefState } from "../../iface";
 import * as API from "../../common/ifaces";
+
+import BaseComponent from "../BaseComponent";
 
 interface PhotoListProps extends IDefProps {
     dir: string
 }
 
-interface PhotoListStats {
+interface PhotoListStats extends IDefState {
     photolist: API.RespPhotoList,
-    error: string
 }
 
-export default class PhotoTaggerPhotolist extends Component<PhotoListProps, PhotoListStats> {
-    constructor() {
-        super();
+export default class PhotoTaggerPhotolist extends BaseComponent<PhotoListProps, PhotoListStats> {
+    constructor(p: PhotoListProps, ctx: any) {
+        super(p, ctx);
         this.state = {
             photolist: null,
-            error: null
         };
     }
 
+    fetchPhotoList(dir: string) {
+        this.download("photo list", `/api/photos/photos/${dir}`)
+            .then((res: API.RespPhotoList) => {
+                this.setState({ photolist: res })
+            });
+    }
+
     componentDidMount() {
-        this.componentDidUpdate({dir:null});
+        this.componentDidUpdate({ dir: null });
     }
 
     componentDidUpdate(prevProps: PhotoListProps) {
-        if(prevProps.dir == this.props.dir) return;
-        
-        fetch(`/api/photos/photos/${this.props.dir}`, {
-            method: "GET",
-            cache: "no-cache"})
-        .then(res => res.json())
-        .then( (res: API.APIResponse<API.RespPhotoList>) => {
-            if (res.result == API.APIResponseResult.Fail) {
-                this.setState({photolist: null, error: res.resultDetail });
-            } else {
-                this.setState({photolist: res.data, error: null })
-            }
-        });
+        if (prevProps.dir == this.props.dir) return;
+
+        this.fetchPhotoList(this.props.dir);
+
     }
 
     //////////////////////////////
     /*          RENDER          */
     //////////////////////////////
-    render() {
-        const {photolist, error } = this.state;
-
-        if(photolist == null && error == null) {
-            return <h1>Loading photo list ...</h1>
-        } else if (photolist == null && error != null) {
-            return <h1>Error: {error}</h1>
-        } else if (error != null && photolist != null) {
-            return <h1>PhotoList WTF state</h1>;
-        } else {
-            return <div>
-                <a href="/photos/">Directory list</a>
-                <ul>
-                {photolist.map(itm => 
+    r() {
+        const { photolist } = this.state;
+        if (!photolist) return <h1>Loading</h1>;
+        return <div>
+            <a href="/photos/">Directory list</a>
+            <ul>
+                {photolist.map(itm =>
                     <a href={`/photos/tag/${itm}`}><img src={`/api/photos/photo/${itm}/thumb`} width={256} /></a>
-                    )
+                )
                 }
-                </ul></div>
-        }
-        
+            </ul>
+        </div>
     }
 }
