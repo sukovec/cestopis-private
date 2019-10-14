e5bd43ee0a382931a8aab1646cf1ebf7e7fc5d88
Michal Soucek <inasuk@gmail.com>
2019-10-14 09:41:32 +0200

    Added forgotten file

---
+++ backend/app/middleware/errormw.ts
@@ -0,0 +1,22 @@
+import { BaseMiddleware } from "inversify-express-utils";
+import { provide } from "inversify-binding-decorators";
+
+import * as express from "express";
+
+import TYPES from "../const/types";
+import * as API from "../common/ifaces";
+
+export default function errorMiddleware(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+    if (!res.headersSent) {
+        res.status(500);
+    }
+
+    let body: API.APIResponse<void> = {
+        result: API.APIResponseResult.Fail,
+        resultDetail: err,
+        data: undefined
+    };
+
+    res.send(body);
+    next();
+}
