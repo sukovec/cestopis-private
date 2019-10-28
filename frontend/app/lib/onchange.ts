ab1f7d892d35d077103ad19675902480f8778e27
Michal Soucek <inasuk@gmail.com>
2019-10-28 05:15:07 +0100

    Created function for handling checkbox change

--- frontend/app/lib/onchange.ts
+++ frontend/app/lib/onchange.ts
@@ -8,3 +8,11 @@ export function HIValue<TProp, TState, TComp extends Component<TProp,TState>>(co
         component.setState(st);
     };
 }
+
+export function HIChecked<TProp, TState, TComp extends Component<TProp, TState>>(component: TComp, stateVar: keyof TState) {
+    return (evt: Event) => {
+        let st: any = {};
+        st[stateVar] = (evt.target as HTMLInputElement).checked;
+        component.setState(st);
+    };
+}
